a3e1d38b9443fb53f7fc944bb53f42e01fb93f74
achchuthan-suntharalingam <achchu0103@gmail.com>
2023-05-01 22:16:22 +0530

    V1.1

--- frontend/src/components/auth/RegisterModal.jsx
+++ frontend/src/components/auth/RegisterModal.jsx
@@ -51,11 +51,44 @@ const handleRegister = () => {
         <Modal size="lg" show={registerShow}  onHide={handleRegisterClose} aria-labelledby="example-modal-sizes-title-lg" >
           <Modal.Header>
             <Modal.Title id="example-modal-sizes-title-lg">
-              Large Modal
+              Register
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <Form>
+              <div class="form-group">
+                <Form.Label for="first-name">First Name:</Form.Label>
+                <Form.Control type="text" class="form-control" id="first-name" placeholder="Enter first name"/>
+              </div>
+              <div class="form-group">
+                <Form.Label for="last-name">Last Name:</Form.Label>
+                <Form.Control type="text" class="form-control" id="last-name" placeholder="Enter last name"/>
+              </div>
+              <div class="form-group">
+                <Form.Label for="mobile-number">Mobile Number:</Form.Label>
+                <Form.Control type="tel" class="form-control" id="mobile-number" placeholder="Enter mobile number"/>
+              </div>
+              <div class="form-group">
+                <Form.Label for="email">Email:</Form.Label>
+                <Form.Control type="email" class="form-control" id="email" placeholder="Enter email"/>
+              </div>
+              <div class="form-group">
+                <Form.Label for="shipping-address">Shipping Address:</Form.Label>
+                <textarea class="form-control" id="shipping-address" rows="3"></textarea>
+              </div>
+              <div class="form-group">
+                <Form.Label for="city">City:</Form.Label>
+                <Form.Control type="text" class="form-control" id="city" placeholder="Enter city"/>
+              </div>
+              <div class="form-group">
+                <Form.Label for="zipcode">Zipcode:</Form.Label>
+                <Form.Control type="text" class="form-control" id="zipcode" placeholder="Enter zipcode"/>
+              </div>
+              <div class="form-group">
+                <Form.Label for="password">Password:</Form.Label>
+                <Form.Control type="password" class="form-control" id="password" placeholder="Enter password"/>
+              </div>
+              <button type="submit" class="btn btn-primary" onClick={handleRegister}>Register</button>
 
             </Form>
           </Modal.Body>
