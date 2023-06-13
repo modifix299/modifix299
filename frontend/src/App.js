84ae5f5bd1856c06a3a8cfe9a91f32c46c6d7158
achchuthan-suntharalingam <achchu0103@gmail.com>
2023-06-13 21:29:40 +0530

    V_5.1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,11 +20,11 @@ function App() {
             <Route path='/' element={<Layout/>}>
                 <Route index element={<Home />}/>
                 <Route path='about' element={<About/>} />
-                <Route path='products' element={<Products/>} />                   
+                <Route path='products' element={<Products/>} /> 
+                <Route path='product/:id' element={<ProductView/>} />                  
                 
                 {/* protected routes */}
-                <Route element={<AuthChecker/>}> 
-                  <Route path='product/:id' element={<ProductView/>} />            
+                <Route element={<AuthChecker/>}>                              
                   <Route path='cart' element={<Cart/>} />
                   <Route path='checkout' element={<Checkout/>} />                  
                   <Route path='profile' element={<Profile/>} />
