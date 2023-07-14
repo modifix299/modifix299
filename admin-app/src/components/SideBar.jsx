3b404864032a640acc0b17b8eaa04b16474a22fe
achchuthan suntharalingam <achchu0103@gmail.com>
2023-07-14 20:39:54 +0530

    logo-styling

--- admin-app/src/components/SideBar.jsx
+++ admin-app/src/components/SideBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from "react-router-dom";
-import logo from '../img/modifix.png';
+// import logo from '../img/modifix.png';
 import { useSelector } from 'react-redux';
 
 const SideBar = () => {    
