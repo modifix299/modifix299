5bd0fe5b6d66fb665b56e02998e45d85c87df922
achchuthan suntharalingam <achchu0103@gmail.com>
2023-07-17 10:55:07 +0530

    alwdOrgns

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,8 +1,8 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:5000',
-    'https://modifix.netlify.app/',
-    'https://modifix-admin.netlify.app/'
+    'https://modifix.netlify.app',
+    'https://modifix-admin.netlify.app'
 ];
 
 module.exports = allowedOrigins;
